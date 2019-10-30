49f72e40975670ccf71012e21a4cebcc7378925b
Shpakova Alena <shpakovadev@gmail.com>
2019-10-30 11:07:22 +0300

    HTML Form component

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const Form = props => {
-  return <div></div>
+  return (
+    <form>
+      <input type="text" placeholder="Enter city" name="city" />
+      <button>Search</button>
+    </form>
+  )
 }
 
 export default Form
